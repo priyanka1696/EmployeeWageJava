59f8b8b47c7257206766cde108d0e423757a8de9
Priyanka <fulmogarepriya@gmail.com>
2020-05-11 12:58:22 +0530

    Added Part Time Employee & Wage

--- EmployeeWageJava.java
+++ EmployeeWageJava.java
@@ -3,6 +3,7 @@ public class EmployeeWageJava {
          System.out.println("Welcome to Employee Wage..");
 
 	int IsFullTime=1;
+	int IsPartTime=2;
 	int empRatePerHrs=20;
 	int empHrs=0;
 	int empwage=0;
@@ -10,6 +11,8 @@ public class EmployeeWageJava {
 	double empcheck=Math.floor(Math.random() * 10)%2;
 	if ( empcheck == IsFullTime)
 		empHrs=8;
+	else if ( empcheck == IsPartTime )
+		empHrs=4;
 
 		empwage=empHrs*empRatePerHrs;
 		System.out.println("Employee Wage="+empwage);
