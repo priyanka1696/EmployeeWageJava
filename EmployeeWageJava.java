cda00d46c2c24a19a93ecfb9bcdb7a3490a5e5d2
priyanka1696 <63593675+priyanka1696@users.noreply.github.com>
2020-05-12 11:33:48 +0530

    Added Working Days

--- EmployeeWageJava.java
+++ EmployeeWageJava.java
@@ -4,15 +4,17 @@ public class EmployeeWageJava {
         	public static final int IsPartTime=2;
   	        public static final int empRatePerHrs=20;
 		public static final int NumberOfWorkingDays=20;
+		public static final int MaxHrsInMonth=10;
      public static void main(String[] args) {
          System.out.println("Welcome to Employee Wage..");
 
-	int TotalEmpWage=0;
-	int EmpWage=0;
+	int TotalEmpHrs=0;
 	int EmpHrs=0;
+	int TotalWorkingDays=0;
 
-for (int day=0;day<NumberOfWorkingDays;day++) 
+while (TotalEmpHrs <= MaxHrsInMonth && TotalWorkingDays < NumberOfWorkingDays) 
 	{
+	   TotalWorkingDays++;
 	   int empcheck=((int)Math.floor(Math.random() * 10))%3;
 		switch (( empcheck ))
 		 {
@@ -25,11 +27,11 @@ for (int day=0;day<NumberOfWorkingDays;day++)
 		     default:
 			EmpHrs=0;
 		}
-	     }
-
-		EmpWage=EmpHrs*empRatePerHrs;
-		TotalEmpWage+=EmpWage;
-		System.out.println("Employee Wage="+TotalEmpWage);
+	     	
+		TotalEmpHrs+=EmpHrs;
+		System.out.println("Day#: " + TotalWorkingDays + "EmpHrs: " +EmpHrs);
 
 	}
+	     int TotlEmpWage = TotalEmpHrs * empRatePerHrs;
+	     System.out.println("Total Emp Wage: " +TotalEmpWage);
 }
